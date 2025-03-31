4f17b0b0299dd3c802ab4bfb6cf5f44930b350ab
Yuliia Romanovych <jdani22101998@gmail.com>
2025-04-01 00:23:18 +0300

    updated db model

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -15,7 +15,10 @@ const contactSchema = new Schema(
       default: 'personal',
     },
   },
-  { timestamps: true },
+  { 
+    timestamps: true,
+    versionKey: false, // Disable the __v field
+  },
 );
 
 const Contact = model('Contact', contactSchema);
